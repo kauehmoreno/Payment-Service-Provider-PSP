38ef6d2981dc0fff7c5bb4375def9befdc73578d
kauehmoreno <kauehmoreno@hotmail.com>
2020-02-04 10:08:24 -0300

    feat:[WIP] Include delete event by date on reaction

--- api/transactions/events.ts
+++ api/transactions/events.ts
@@ -1,14 +1,19 @@
 import { Storager } from "../cache/cache"
 import * as logger from 'bunyan';
-import { Transaction } from "./transactions";
+import { Transaction, transactionCacheKey } from "./transactions";
 
 export const transactionEvent = {
     onCreate: (): string => "transaction_created"
 }
 
 export const onCreate = (cache:Storager,log: logger,tr: Transaction): void => {
-    const key = `cacheKey${tr.id}`
+    const key = `${transactionCacheKey}${tr.id}`
     cache.set<Transaction>(key, tr, JSON.stringify).catch(err=>{
         log.error(err, "could not set transaction on cache")
+    })
+    let dayTransaction = new Date().toJSON()
+    dayTransaction = dayTransaction.split("T")[0]
+    cache.delete(`${transactionCacheKey}${dayTransaction}`).catch(err=>{
+        log.error(err, `could not delete transaction from: ${dayTransaction}`)
     })
 }
