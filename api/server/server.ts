959bd095c7a2efc4e95f571a33337d6939748328
kauehmoreno <kauehmoreno@hotmail.com>
2020-02-03 22:53:17 -0300

    feat: Build server config

---
+++ api/server/server.ts
@@ -0,0 +1,25 @@
+import { Settings } from "../../settings/settings";
+import {Express } from 'express';
+import { Database } from "../db/db";
+import { EventEmitter } from "events";
+import * as logger from 'bunyan';
+
+export interface ServerConf {
+    app:Express 
+    settings: Settings
+    database?: Database
+    event: EventEmitter
+    log: logger
+}
+
+export const serverBuild = (app: Express, settings: Settings): ServerConf => {
+    return {
+        app: app,
+        settings:settings,
+        log: logger.createLogger({
+            name:settings.aplication().appName,
+            level:settings.aplication().logLevel
+        }),
+        event: new EventEmitter()
+    }
+}
