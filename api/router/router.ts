89ca809229202b1c1be1384bc67426671af78b9c
kauehmoreno <kauehmoreno@hotmail.com>
2020-02-04 20:58:59 -0300

    feat: iNCLUDE NEW ROUTERS

--- api/router/router.ts
+++ api/router/router.ts
@@ -1,14 +1,22 @@
 import { ServerConf } from "../server/server";
-import * as express from "express"
 import { middlewareBuilder } from "./middleware";
+import { healthcheckHandler } from "../handler/healthcheck";
+import { transactionCreateHandler } from "../handler/transaction";
 
 export const routerBuilder = (server: ServerConf): void => {
     middlewareBuilder(server)
     healthCheckRouter(server)
+    publicRouter(server)
 }
 
 const healthCheckRouter = (server: ServerConf): void => {
-    server.app.get("/healthcheck", (request: express.Request, response: express.Response)=>{
-        response.json({status:"WORKING"})
-    })
+    server.app.get("/healthcheck", healthcheckHandler(server))
+}
+
+const publicRouter = (server: ServerConf): void => {
+    transactionRouter(server)
+}
+
+const transactionRouter = (server: ServerConf): void => {
+    server.app.post("/transanction", transactionCreateHandler(server))
 }
