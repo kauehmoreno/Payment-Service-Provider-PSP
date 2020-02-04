dfcfdee2af1261b89b0537e178362058bb3ce476
kauehmoreno <kauehmoreno@hotmail.com>
2020-02-03 23:29:21 -0300

    test: Create tests to basic storage behavior

---
+++ api/cache/cache.test.ts
@@ -0,0 +1,50 @@
+import * as mockRedis from "redis-mock"
+import {v4 as uuid} from "uuid"
+import { newStorage } from "./cache"
+
+interface testDoc {
+    name:string
+    status:string
+}
+
+describe("storager test case",()=>{
+    const storager  = newStorage(mockRedis.createClient())
+    describe("set cases",()=>{
+        test("should return boolean true when insert value", async()=>{
+            const doc = {
+                name:"test cache document",
+                status:"OK"
+            }
+            const ok = await storager.set<testDoc>("test insert", doc, JSON.stringify)
+            expect(ok).toBeTruthy()
+        })
+        test("set a string instead of object should not throw error on invalidate operation",async()=>{
+            const ok = await storager.set<string>("test:string","my test string",JSON.stringify)
+            expect(ok).toBeTruthy()
+        })
+        test("should return false whenever something got wrong",async()=>{
+            const ok = await storager.set<string>("test:string","my test string",(text: string, reviver?: ((this: any, key: string, value: any) => any) | undefined):any =>{
+                throw new Error("my internal error")
+            })
+            expect(ok).toBeFalsy()
+        })
+    })
+    describe("get case",()=>{
+        test("should return an item whenever finds one",done=>{
+            storager.get<testDoc>("test insert", (err:Error|null, reply:any) => {
+                expect(err).toBeNull()
+                const doc = JSON.parse(reply)
+                expect(doc.name).toBe("test cache document")
+                expect(doc.status).toBe("OK")
+                done()
+            })
+        })
+        test("should not return error on callback when does not find a item, instead reply is null", done=>{
+            storager.get<testDoc>("not-found", (err:Error|null, reply:any) => {
+                expect(err).toBeNull()
+                expect(reply).toBeNull()
+                done()
+            })
+        })
+    })
+})
