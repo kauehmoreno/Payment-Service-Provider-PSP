dbd6bfd3abed5963b85bbc1c82f7d4637f1a938a
kauehmoreno <kauehmoreno@hotmail.com>
2020-02-04 20:59:31 -0300

    feat:[WIP] Add events

---
+++ api/server/events.ts
@@ -0,0 +1,9 @@
+import { ServerConf } from "./server";
+import { transactionEvent, onCreate } from "../transactions/events";
+import { Transaction } from "../transactions/transactions";
+
+export const registerEvent = (server:ServerConf): void => {
+    server.event.on(transactionEvent.onCreate(), (tr:Transaction)=>{
+        server.storage ? onCreate(server.storage, server.log, tr): null
+    })
+}
