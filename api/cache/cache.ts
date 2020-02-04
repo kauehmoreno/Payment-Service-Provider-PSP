903151f676bce6b95e214758168516ab280519b0
kauehmoreno <kauehmoreno@hotmail.com>
2020-02-03 23:29:32 -0300

    feat: implement storager

---
+++ api/cache/cache.ts
@@ -0,0 +1,82 @@
+import { ServerConf } from "../server/server"
+import * as provider from 'redis';
+import { resolve } from "dns";
+
+
+type Marshaller = (value: any, replacer?: ((this: any, key: string, value: any) => any) | undefined, space?: string | number | undefined) => string
+type CallbackResponser<T> = (err:Error|null, reply:any) => any
+
+export interface Storager{
+    get<T>(key:string, cb:CallbackResponser<T>): Promise<T>
+    set<T>(key:string, value: T, marshaller:Marshaller): Promise<boolean>
+}
+
+export interface StorageSetup {
+    host: string
+    port:number
+    keepalive: boolean
+    connectTimeout: number
+    password:string
+}
+
+
+type config = (storage: StorageSetup) => void
+
+export const withHost = (host: string):config => {
+    return function(s: StorageSetup){
+        s.host = host
+    }
+}
+export const withPort = (port: number):config => {
+    return function(s: StorageSetup){
+        s.port = port
+    }
+}
+export const withKeepAlive = (keepAlive: boolean):config => {
+    return function(s: StorageSetup){
+        s.keepalive = keepAlive
+    }
+}
+export const withConnTimeout = (timeout: number):config => {
+    return function(s: StorageSetup){
+        s.connectTimeout = timeout
+    }
+}
+
+export const withPassword = (password: string):config => {
+    return function(s: StorageSetup){
+        s.password = password
+    }
+}
+
+export const connectStorage = (...configs: config[]): Storager => {
+    let storageSetup: StorageSetup = {} as StorageSetup
+    configs.forEach(config => config(storageSetup))
+
+        const cli = provider.createClient({
+            host: storageSetup.host,
+            port: storageSetup.port,
+            socket_keepalive: storageSetup.keepalive,
+            connect_timeout: storageSetup.connectTimeout,
+            password:storageSetup.password,
+        })
+
+    return newStorage(cli)
+}
+
+
+export const newStorage = (cli: provider.RedisClient): Storager => {
+    return {
+        get: async <T>(key: string, cb: CallbackResponser<T>): Promise<any> => {
+            return cli.get(key, cb)
+        },
+        set: async <T>(key: string, value:T, marshaller: Marshaller): Promise<boolean> => {
+            try{
+                cli.set(key, marshaller(value))
+                return Promise.resolve(true)
+            }catch(err){
+                return Promise.resolve(false)
+            }
+        }
+    }
+}
