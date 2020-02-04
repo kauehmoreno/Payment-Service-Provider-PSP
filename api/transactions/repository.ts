d3b741b6be26f8fe84155a0feb8bc0f702ef3668
kauehmoreno <kauehmoreno@hotmail.com>
2020-02-04 01:06:27 -0300

    feat:[WIP] Create saveTransaction

--- api/transactions/repository.ts
+++ api/transactions/repository.ts
@@ -1,4 +1,24 @@
+import { Transaction } from "./transactions";
+import { Storager } from "../cache/cache";
+import { Writer } from "../db/db";
+import { EventEmitter } from "events";
+import { validate, valueValidator, paymentMethodValidator } from "./rules";
 
+enum tableName {
+    name = "transaction"
+}
 
-
-// export const saveTransaction = ()
+export const saveTransaction = async (transaction: Transaction, db:Writer, notifer?:EventEmitter): Promise<string> => {
+    const error = validate(transaction, valueValidator(), paymentMethodValidator())
+    if(error){
+        throw error
+    }
+    try{
+        const result = await db.insert(tableName.name, [transaction])
+        transaction.id = result[0]
+        notifer?.emit(transactionEvent.onCreate())
+    }catch(err){
+        throw new Error(`could not save transaction [${err.name}]: ${err.message}`)
+    }
+    return ""
+}
