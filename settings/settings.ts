968f7a74a738dd09886f14ada14b9afb13f135ca
kauehmoreno <kauehmoreno@hotmail.com>
2020-02-03 22:44:19 -0300

    feat: Create settings which is responsible to retrieve setup and confi valeus

---
+++ settings/settings.ts
@@ -0,0 +1,56 @@
+const databaseSettings = (): any => {
+    const env = process.env;
+    return {
+        options:{
+            poolSize: 10,
+            tls: false,
+            keepAlive:true,
+            noDelay:true,
+            useUnifiedTopology: true,
+            useNewUrlParser:true,
+            auth:{
+                user: env.DB_LOGIN,
+                password: env.DB_PASSWORD,
+            },
+        },
+        appName:appSettings().appName,
+        url: env.DB_URL,
+        testUrl:"mongodb://localhost:27017",
+        database:env.DB_DATABASE,
+        port:27017,
+    }
+}
+
+const appSettings = (): any => {
+    const env = process.env;
+    return {
+        appName: env.APP_NAME??"jacortei",
+        port: env.APP_PORT??8080,
+        host: env.APP_HOST,
+        logLevel: env.LOG_LEVEL??"info"
+    }
+}
+
+const cacheSettings = (): any => {
+    const env = process.env;
+    return {
+        host: env.CACHE_HOST??"",
+        port: env.CACHE_PORT??"",
+        keepAlive: env.CACHE_KEEPALIVE??true,
+        timeout: env.CACHE_TIMEOUT,
+        password: env.CACHE_PASSWORD,
+    }
+}
+
+export class Settings {
+    database():any {
+        return databaseSettings();
+    } 
+    aplication(): any{
+        return appSettings();
+    }
+
+    cache(): any {
+        return cacheSettings()
+    }
+};
