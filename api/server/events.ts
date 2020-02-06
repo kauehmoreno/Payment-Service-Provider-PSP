39ca5c585ccc167744c177545e021dd42eca3565
Kauêh Moreno <kaueh.rodrigues@g.globo>
2020-02-06 18:03:38 -0300

    feat: Include queue on parans to transaciont created

--- api/server/events.ts
+++ api/server/events.ts
@@ -4,6 +4,6 @@ import { Transaction } from "../transactions/transactions";
 
 export const registerEvent = (server:ServerConf): void => {
     server.event.on(transactionEvent.onCreate(), (tr:Transaction)=>{
-        server.storage ? onCreate(server.storage, server.log, tr): null
+        server.storage ? onCreate(server.storage, server.log ,server.queue, tr): null
     })
 }
