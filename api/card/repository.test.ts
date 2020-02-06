8440b9f77a6bb328a2c2160bd64a72d314f2bee1
Kauêh Moreno <kaueh.rodrigues@g.globo>
2020-02-06 10:36:02 -0300

    test: Create test to cover card repositry cases

--- api/card/repository.test.ts
+++ api/card/repository.test.ts
@@ -1,5 +1,5 @@
 import { newCard, withCardNumber, withCardName, withExpireAt, withCardCvv,Card } from "./card"
-import { saveCard } from "./repository"
+import { saveCard, cardById } from "./repository"
 import { ObjectId } from "mongodb"
 
 const mockDb = {
@@ -63,6 +63,47 @@ describe("card repository",()=>{
                     return Promise.resolve([new ObjectId().toHexString()])
                 })
                 await saveCard(card, mockDb)
+            })
+        })
+    })
+    describe("card byId",()=>{
+        describe("success cases",()=>{
+            test("should return data from cache whenever hits one",async()=>{
+                const card = newCard(withCardNumber("5168441223630339"),withCardName("MY CARD NAME"),
+                        withExpireAt("09/2027"),withCardCvv(233))
+                 mockStorager.get.mockImplementation((key:string,cb:(error:Error|null,reply:any)=>void)=>{
+                     cb(null, JSON.stringify(card))
+                 })
+
+                const result = await cardById("3213123123",mockStorager,mockDb)
+                expect(result?._id).toBe(card._id)
+            })
+            test("should retrieve item on db whenever cache does not find or fail and it cache back to set value",async()=>{
+                const card = newCard(withCardNumber("5168441223630339"),withCardName("MY CARD NAME"),
+                        withExpireAt("09/2027"),withCardCvv(233))
+                 mockStorager.get.mockImplementation((key:string,cb:(error:Error|null,reply:any)=>void)=>{
+                     cb(null, null)
+                 })
+                 const id = new ObjectId()
+                 card._id = id
+                 mockDb.get.mockResolvedValue(card)
+                 const result = await cardById(id.toHexString(),mockStorager,mockDb)
+                 expect(result?._id.toHexString()).toBe(id.toHexString())
+                 expect(mockStorager.set.call.length).toBe(1)
+            })
+            test("should not return error if fails to set on cache",async()=>{
+                const card = newCard(withCardNumber("5168441223630339"),withCardName("MY CARD NAME"),
+                withExpireAt("09/2027"),withCardCvv(233))
+                mockStorager.get.mockImplementation((key:string,cb:(error:Error|null,reply:any)=>void)=>{
+                    cb(null, null)
+                })
+                const id = new ObjectId()
+                card._id = id
+                mockDb.get.mockResolvedValue(card)
+                mockStorager.set.mockRejectedValue(new Error("fail to set value into cache"))
+                const result = await cardById(id.toHexString(),mockStorager,mockDb)
+                expect(result?._id.toHexString()).toBe(id.toHexString())
+                expect(mockStorager.set.call.length).toBe(1) 
             })
         })
     })
