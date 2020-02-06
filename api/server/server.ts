9a7d1871ddee8c93cb0f61926fa21147475f68cd
Kauêh Moreno <kaueh.rodrigues@g.globo>
2020-02-06 16:53:32 -0300

    feat: Include queue

--- api/server/server.ts
+++ api/server/server.ts
@@ -4,6 +4,8 @@ import { EventEmitter } from "events";
 import * as logger from 'bunyan';
 import { Storager } from "../cache/cache";
 import { Settings } from '../settings/settings';
+import { Queue } from '../queue/queue';
+
 
 export interface ServerConf {
     app:Express 
@@ -11,17 +13,33 @@ export interface ServerConf {
     database?: Database
     storage?: Storager
     event: EventEmitter
+    queue: Queue
     log: logger
 }
 
-export const serverBuild = (app: Express, settings: Settings): ServerConf => {
-    return {
+type config = (s: ServerConf) => void
+
+
+export const serverWithQueue = (q: Queue): config =>{
+    return (s:ServerConf) => {
+        s.queue = q
+    }
+}
+
+export const serverBuild = (app: Express, settings: Settings, ...config:config[]): ServerConf => {
+    const server = {
         app: app,
         settings:settings,
         log: logger.createLogger({
             name:settings.aplication().appName,
             level:settings.aplication().logLevel
         }),
-        event: new EventEmitter()
+        event: new EventEmitter(),
+        queue: {} as Queue
     }
+
+    config.forEach(conf => {
+        conf(server)
+    });
+    return server
 }
