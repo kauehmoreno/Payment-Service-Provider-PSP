5f7007e9e1fce4573920bf6a29fd1bc454fcd1c5
Kauêh Moreno <kaueh.rodrigues@g.globo>
2020-02-06 18:02:21 -0300

    feat: Include notification on queue on transaction created

--- api/transactions/events.ts
+++ api/transactions/events.ts
@@ -1,3 +1,4 @@
+import { Queue } from "../queue/queue"
 import { Storager } from "../cache/cache"
 import * as logger from 'bunyan';
 import { Transaction, transactionCacheKey } from "./transactions";
@@ -6,7 +7,7 @@ export const transactionEvent = {
     onCreate: (): string => "transaction_created"
 }
 
-export const onCreate = (cache:Storager,log: logger,tr: Transaction): void => {
+export const onCreate = (cache:Storager,log: logger, queue:Queue, tr: Transaction): void => {
     const key = `${transactionCacheKey}${tr._id.toHexString()}`
     cache.set<Transaction>(key, tr, JSON.stringify).catch(err=>{
         log.error(err, "could not set transaction on cache")
@@ -16,4 +17,9 @@ export const onCreate = (cache:Storager,log: logger,tr: Transaction): void => {
     cache.delete(`${transactionCacheKey}${dayTransaction}`).catch(err=>{
         log.error(err, `could not delete transaction from: ${dayTransaction}`)
     })
+    try{
+        queue.publish(transactionEvent.onCreate(), tr)
+    }catch(err){
+        log.error(`could not queue ${tr._id}: [${err.name}]: ${err.message}`)
+    }
 }
