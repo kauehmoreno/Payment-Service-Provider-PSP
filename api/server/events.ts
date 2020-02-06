e451d8abe6be6399826a4d54b2d417a673d9d284
Kauêh Moreno <kaueh.rodrigues@g.globo>
2020-02-06 18:05:46 -0300

    feat: Register event of payable

--- api/server/events.ts
+++ api/server/events.ts
@@ -1,9 +1,15 @@
 import { ServerConf } from "./server";
 import { transactionEvent, onCreate } from "../transactions/events";
 import { Transaction } from "../transactions/transactions";
+import { payableEvent,onCreate as oncreatePayable } from "../payable/event";
+import { Payable } from "../payable/payable";
 
 export const registerEvent = (server:ServerConf): void => {
     server.event.on(transactionEvent.onCreate(), (tr:Transaction)=>{
         server.storage ? onCreate(server.storage, server.log ,server.queue, tr): null
+    })
+
+    server.event.on(payableEvent.onCreate(),(payable:Payable)=>{
+        server.storage ? oncreatePayable(server.storage,server.log, payable) : null
     })
 }
