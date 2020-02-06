6a2bfb68197c7e7ffe3a3ca688cad7090a05b4a7
Kauêh Moreno <kaueh.rodrigues@g.globo>
2020-02-06 19:07:20 -0300

    refactor: Check for database first and return error if something happens

--- api/handler/transaction.ts
+++ api/handler/transaction.ts
@@ -17,34 +17,37 @@ export const transactionCreateHandler = (s:ServerConf): Handler => {
             return
         }
 
+        if(!s.database){
+            s.log.error(`database is not provided`)
+            writeResponse(resp, withStatusCode(500),withError("something wrong happens"))
+            return  
+        }
         const transaction = createTransaction(body.description, withValue(body.value), withMethod(body.paymentMethod))
         const card = newCard(withCardNumber(body.card.number),withCardName(body.card.name), 
             withExpireAt(body.card.expireAt), withCardCvv(body.card.cvv))
-        if(s.database){
+        try{
+            const cardId = await saveCard(card, s.database)
+            transaction.cardId = cardId
             try{
-                const cardId = await saveCard(card, s.database)
-                transaction.cardId = cardId
-                try{
-                    const trId = await saveTransaction(transaction,s.database,s.event)
-                    transaction._id = new ObjectId(trId)
-                    writeResponse(resp, withStatusCode(200),withData({transactionId:trId, status:"created"}))
-                    return
-                }catch(error){
-                    s.log.error(`transaction: [${error.name}]:${error.message}`)
-                    if(error.code && error.code == validatorErrCode){
-                        writeResponse(resp, withStatusCode(400),withError("invalid body request to transaction"))
-                        return
-                    }
-                    writeResponse(resp, withStatusCode(500),withError("something got wrong"))
-                }
+                const trId = await saveTransaction(transaction,s.database,s.event)
+                transaction._id = new ObjectId(trId)
+                writeResponse(resp, withStatusCode(200),withData({transactionId:trId, status:"created"}))
+                return
             }catch(error){
-                s.log.error(`card: [${error.name}]:${error.message}`)
+                s.log.error(`transaction: [${error.name}]:${error.message}`)
                 if(error.code && error.code == validatorErrCode){
-                    writeResponse(resp, withStatusCode(400),withError("invalid body request to card"))
+                    writeResponse(resp, withStatusCode(400),withError("invalid body request to transaction"))
                     return
                 }
-                writeResponse(resp, withStatusCode(400),withError("invalid body request"))
+                writeResponse(resp, withStatusCode(500),withError("something got wrong"))
             }
+        }catch(error){
+            s.log.error(`card: [${error.name}]:${error.message}`)
+            if(error.code && error.code == validatorErrCode){
+                writeResponse(resp, withStatusCode(400),withError("invalid body request to card"))
+                return
+            }
+            writeResponse(resp, withStatusCode(400),withError("invalid body request"))
         }
     }
 }
