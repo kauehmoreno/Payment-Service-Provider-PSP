2b94e2bd9840fab2db68a7c5824663361fb50776
Kauêh Moreno <kaueh.rodrigues@g.globo>
2020-02-06 21:35:04 -0300

    feat: Include backoff on setup

--- worker/main.ts
+++ worker/main.ts
@@ -3,12 +3,10 @@ import { Settings } from "../api/settings/settings"
 import * as logger from 'bunyan';
 import { connectQueue, withQueueURL } from '../api/queue/queue';
 import { connDB, withUrl, withConnectionOpts, withDatabase } from '../api/db/db';
-import { serverBuild } from '../api/server/server';
-import { application } from 'express';
-import { routerBuilder } from '../api/router/router';
 import { transactionEvent } from '../api/transactions/events';
 import { Payable } from '../api/payable/payable';
 import { savePayable } from '../api/payable/repository';
+import { backoff } from '../api/core/backoff';
 
 const envSetup = () => {
     const result = env.config()
@@ -25,34 +23,24 @@ const log = logger.createLogger({
     level:settings.aplication().logLevel
 })
 
-const retry = (retries: number, fn: () => Promise<any>) => {
-    fn().catch(err => retries > 1 ? retry(retries - 1, fn) : Promise.reject(err));
-}
-
-const pause = (duration:number) => new Promise(res => setTimeout(res, duration));
-
-const backoff = (retries:number, fn: () => Promise<any>, delay = 500) =>{
-    fn().catch(err => retries > 1
-      ? pause(delay).then(() => backoff(retries - 1, fn, delay * 2))
-      : Promise.reject(err));
-}
-
 connectQueue(withQueueURL(settings.queue().url)).then(queue => {
     const dbSettings = settings.database()
-    connDB(withUrl(dbSettings.url), withConnectionOpts(dbSettings.options), withDatabase(dbSettings.databae)).then(db =>{
-        queue.subscribe(transactionEvent.onCreate(), (error:Error|null, msg:Payable)=>{
-            if(error){
-                log.error(`fail to subscribe [${error.name}]:${error.message}`)
-                process.abort() 
-            }
-            backoff(10, async () => {
-                return await savePayable(msg, db)
-            }, 10)
+    backoff(10, async ()=> {
+        connDB(withUrl(dbSettings.url), withConnectionOpts(dbSettings.options), withDatabase(dbSettings.databae)).then(db =>{
+            queue.subscribe(transactionEvent.onCreate(), (error:Error|null, msg:Payable)=>{
+                if(error){
+                    log.error(`fail to subscribe [${error.name}]:${error.message}`)
+                    process.abort() 
+                }
+                backoff(10, async () => {
+                    return await savePayable(msg, db)
+                }, 10)
+            })
+        }).catch(err => {
+            log.error(`fail to config db on app setup: [${err.name}]:${err.message}`)
+            throw err
         })
-    }).catch(err => {
-        log.error(`fail to config db on app setup: [${err.name}]:${err.message}`)
-        throw err
-    })
+    },10)
 }).catch(err => {
     log.error(`fail to connect with queue [${err.name}]:${err.message}`)
     throw err
