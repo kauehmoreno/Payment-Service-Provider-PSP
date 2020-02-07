f8a53cacc5e3de5f1bb77d2ca505f3249d0841d4
Kauêh Moreno <kaueh.rodrigues@g.globo>
2020-02-06 22:53:34 -0300

    fix: Receive transaction and create payable

--- worker/main.ts
+++ worker/main.ts
@@ -4,9 +4,10 @@ import * as logger from 'bunyan';
 import { connectQueue, withQueueURL } from '../api/queue/queue';
 import { connDB, withUrl, withConnectionOpts, withDatabase } from '../api/db/db';
 import { transactionEvent } from '../api/transactions/events';
-import { Payable } from '../api/payable/payable';
+import { Payable, newPayable } from '../api/payable/payable';
 import { savePayable } from '../api/payable/repository';
 import { backoff } from '../api/core/backoff';
+import { Transaction } from '../api/transactions/transactions';
 
 const envSetup = () => {
     const result = env.config()
@@ -27,14 +28,17 @@ connectQueue(withQueueURL(settings.queue().url)).then(queue => {
     const dbSettings = settings.database()
     backoff(10, async ()=> {
         connDB(withUrl(dbSettings.url), withConnectionOpts(dbSettings.options), withDatabase(dbSettings.databae)).then(db =>{
-            queue.subscribe(transactionEvent.onCreate(), (error:Error|null, msg:Payable)=>{
+            queue.subscribe(transactionEvent.onCreate(), (error:Error|null, msg:Transaction)=>{
                 if(error){
                     log.error(`fail to subscribe [${error.name}]:${error.message}`)
                     process.abort() 
                 }
-                backoff(10, async () => {
-                    return await savePayable(msg, db)
-                }, 10)
+                const payable = newPayable(msg)
+                savePayable(payable, db).catch(err => {
+                    backoff(10, async () => {
+                        return await savePayable(payable, db)
+                    }, 10) 
+                })
             })
         }).catch(err => {
             log.error(`fail to config db on app setup: [${err.name}]:${err.message}`)
