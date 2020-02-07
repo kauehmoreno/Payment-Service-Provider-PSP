7b3c60ec4e0a36324c1a00b422fe3d70bb5ef173
Kauêh Moreno <kaueh.rodrigues@g.globo>
2020-02-07 09:12:33 -0300

    test: Fix tests of new payable rules and create other cases

--- api/payable/rules.test.ts
+++ api/payable/rules.test.ts
@@ -1,4 +1,4 @@
-import { statusModified, runRules, payableModified, taxesModified } from "./rules"
+import { statusModified, runRules, payableModified, taxesModified, buildPaybleBalance } from "./rules"
 import { paymentMethod, createTransaction, withClientId, withValue, withCard, withMethod } from "../transactions/transactions"
 import { ObjectId } from "mongodb"
 import { newPayable, Payable, Status } from "./payable"
@@ -68,6 +68,52 @@ describe("payable rules",()=>{
             modify(payable)
             expect(payable.taxes).toBe(0.05)
             expect(payable.total).toBe(95)
+        })
+    })
+    describe("generate balance",()=>{
+        const generateWaitingFundsBalance = (v:number): Payable => {
+            const payable = generatePayable(new ObjectId().toHexString(), v)
+            payable.status = Status.waiting
+            return payable
+        }
+        test("should build balance correctly base on payables",()=>{
+            const payables:Payable[] = [
+                generatePayable(new ObjectId().toHexString(), 100), 
+                generatePayable(new ObjectId().toHexString(), 1200), 
+                generateWaitingFundsBalance(1000),
+                generateWaitingFundsBalance(100),
+            ]
+
+            const balance = buildPaybleBalance(...payables)
+            expect(balance.available).toBe(1300)
+            expect(balance.waitingFunds).toBe(1100)
+        })
+        test("should keep floating point and preserve calculation",()=>{
+            const payables:Payable[] = [
+                generatePayable(new ObjectId().toHexString(), 100.44), 
+                generatePayable(new ObjectId().toHexString(), 120.32), 
+                generateWaitingFundsBalance(1000.21),
+                generateWaitingFundsBalance(99.99),
+            ]
+
+            const balance = buildPaybleBalance(...payables)
+            expect(balance.available).toBe(220.76)
+            expect(balance.waitingFunds).toBe(1100.2)
+        })
+        test("an nullable element should change result calculation",()=>{
+            const payables:(Payable|null)[] = [
+                generatePayable(new ObjectId().toHexString(), 100.44), 
+                null,
+                generatePayable(new ObjectId().toHexString(), 120.32), 
+                null,
+                generateWaitingFundsBalance(1000.21),
+                null,
+                generateWaitingFundsBalance(99.99),
+                null,
+            ]
+            const balance = buildPaybleBalance(...payables,null,null)
+            expect(balance.available).toBe(220.76)
+            expect(balance.waitingFunds).toBe(1100.2)
         })
     })
 })
